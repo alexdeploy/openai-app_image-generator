60f4f893f70b8bd583cf4f4b49c89d4a00f9a875
alexruedadev <me@alexrueda.dev>
2023-03-16 16:58:47 +0100

    Adding custom font: san francisco

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -2,7 +2,13 @@
 module.exports = {
   content: ["./index.html", "./src/**/*.{vue,js,ts,jsx,tsx}", ],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        sf_bold: ['San Francisco Bold'],
+        sf_regular: ['San Francisco Regular']
+
+      }
+    },
   },
   plugins: [],
 }
