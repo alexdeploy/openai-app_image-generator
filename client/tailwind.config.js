7a37fb62b9005994a287517bf50dd446a1f69988
alexruedadev <me@alexrueda.dev>
2023-03-20 12:51:12 +0100

    Added some custom colors to tailwind config

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -3,6 +3,10 @@ module.exports = {
   content: ["./index.html", "./src/**/*.{vue,js,ts,jsx,tsx}", ],
   theme: {
     extend: {
+      colors: {
+        dark_blue: '#0f141e',
+        dark_blue_s: '#21242e'
+      },
       fontFamily: {
         sf_bold: ['San Francisco Bold'],
         sf_regular: ['San Francisco Regular']
