be0dd5f8f9522207a1da40871734ce6b8484597b
alexruedadev <me@alexrueda.dev>
2023-03-20 12:51:31 +0100

    Added footer component (empty)

---
+++ client/src/components/Footer.vue
@@ -0,0 +1,20 @@
+<template>
+  <footer class="border-gray-800 border-1 border-t border-dashed bg-black relative">
+    <section>
+        <p>Footer section 1</p>
+    </section>
+    <section>
+        <p>Footer section 2</p>
+    </section>
+  </footer>
+</template>
+
+<script>
+export default {
+    name: 'Footer',
+}
+</script>
+
+<style>
+
+</style>
