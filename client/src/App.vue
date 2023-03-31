a284366fd51bc467b00bbd3bb250a262cb8ecb11
alexruedadev <me@alexrueda.dev>
2023-03-31 18:08:53 +0200

    Added github corner component

--- client/src/App.vue
+++ client/src/App.vue
@@ -2,11 +2,13 @@
 import { RouterView } from 'vue-router'
 import Header from './components/Header.vue'
 import Footer from './components/Footer.vue'
+import GithubCorner from './components/GithubCorner.vue'
 </script>
 
 <template>
   <div>
     <Header />
+    <GithubCorner url="https://github.com/alexdeploy" />
     <RouterView />
     <Footer />
   </div>
