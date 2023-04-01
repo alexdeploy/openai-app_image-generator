28cc204541236efed714064b02e6d69678a04945
alexruedadev <me@alexrueda.dev>
2023-04-01 17:47:53 +0200

    Added simple footer info

--- client/src/components/Footer.vue
+++ client/src/components/Footer.vue
@@ -1,8 +1,16 @@
 <template>
-  <footer class="shadow-sm">
+  <footer class="shadow-sm w-full flex justify-center py-5 flex-col items-center">
     <span>
       Developed by <a href="">@alexdeploy</a>
     </span>
+    <span class="py-4">
+      with
+    </span>
+    <div class="flex">
+      <img src="../assets/icons/vue.svg" alt="" class="w-14">
+      <span class="flex self-center text-2xl px-5"> + </span>
+      <img src="../assets/icons/openai.svg" alt="" class="w-14">
+    </div>
   </footer>
 </template>
 
