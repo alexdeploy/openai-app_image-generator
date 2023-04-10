ad9c9e2e5877c8a1b3596b389f76ef75521bf1be
alexruedadev <me@alexrueda.dev>
2023-04-10 16:10:26 +0200

    Basic image request function

--- client/src/utils/openai.js
+++ client/src/utils/openai.js
@@ -1,5 +1,6 @@
 const GENERATE_IMAGE_URL_DEV = 'http://localhost:5000/openai/generateimage';
 const GENERATE_IMAGE_URL_PROD = 'https://openai-app-image-generator.vercel.app/openai/generateimage';
+const EDIT_IMAGE_URL_DEV = 'http://localhost:5000/openai/editimage';
 
 /**
  * Request to OpenAI API
@@ -34,6 +35,31 @@ async function generateImageRequest(prompt, size){
     }
 }
 
+/**
+ * 
+ * @param {*} imageFile 
+ * @returns 
+ */
+async function editImageRequest(imageFile){
+
+    try {
+        console.log(imageFile)
+        const response = await fetch(EDIT_IMAGE_URL_DEV, {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            body: imageFile
+        })
+
+        return await response.json();
+
+    } catch (error) {
+        console.log(error);
+    }
+}
+
 export {
-    generateImageRequest
+    generateImageRequest,
+    editImageRequest
 }
