e74b4cc5255f0c0e11c407df0774190f2c0651c4
alexruedadev <me@alexrueda.dev>
2023-04-10 16:07:46 +0200

    Added multer and editor route config

--- client/src/router/index.js
+++ client/src/router/index.js
@@ -9,6 +9,11 @@ const router = createRouter({
       name: 'home',
       component: HomeView
     },
+    {
+      path: '/editor',
+      name: 'editor',
+      component: () => import('../views/EditorView.vue')
+    }
   ]
 })
 
